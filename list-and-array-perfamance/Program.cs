9b7e4b69f083ac72ebf707da36a8393532e387fd
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2017-07-15 08:39:40 +0700

    Add project files.

---
+++ list-and-array-perfamance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_and_array_perfamance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
