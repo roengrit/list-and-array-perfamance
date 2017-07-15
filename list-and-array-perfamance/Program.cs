75665221a71262d38c86569bf9bd47cdf0c8e69e
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2017-07-15 08:50:51 +0700

    all of code

--- list-and-array-perfamance/Program.cs
+++ list-and-array-perfamance/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace list_and_array_perfamance
     {
         static void Main(string[] args)
         {
+            List<Int32> listInt = new List<Int32>();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                listInt.Add(i);
+            }
+
+            stopWatch.Stop();
+            Console.Write("Using List : " + stopWatch.ElapsedTicks + "\n");
+            stopWatch.Reset();
+
+
+            stopWatch.Start();
+            Int32[] arr = new Int32[10000];
+            for (int i = 0; i < 10000; i++)
+            {
+                arr[i] = i;
+            }
+            stopWatch.Stop();
+
+            Console.Write("Using Array : " + stopWatch.ElapsedTicks + "\n");
+            Console.ReadLine();
+
+            //// โจทย์นี้จะวัดกันได้เมื่อรู้ขนาดของข้อมูลอยู่แล้ว เพราะฉะนั้นต้องเลือกประเภทข้อมูลให้เหมาะสมกับสถานะการ
         }
     }
 }
